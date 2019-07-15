73cccfa7ec4ae1972e8fe7a14321b12e2b0cfa4a
chrisdews <cldews1@gmail.com>
2019-07-15 17:51:47 +0100

    Done.

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,23 @@
 // your ImageSlider code here!
+
+import React, { Component } from 'react';
+
+class ImageSlider extends Component {
+    constructor(){
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+    
+    render (){
+        return(
+            <p>I am on slide {this.state.currentSlideIndex}</p>
+        )
+    }
+}
+
+export default ImageSlider;
+
+// Its initial state should have a property called currentSlideIndex that starts at 0.
+// It should only render out the text 'I am on slide <CURRENT_SLIDE>', where <CURRENT_SLIDE> is the value of this.state.currentSlideIndex.
